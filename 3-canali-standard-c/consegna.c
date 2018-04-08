d7bb89c448a6bd74abab24193912708fd5aff16a
tony <tony@tony-LIFEBOOK-A544>
2018-04-08 23:04:29 +0200

    consegna

--- 3-canali-standard-c/consegna.c
+++ 3-canali-standard-c/consegna.c
@@ -1,28 +1,30 @@
-# Esercitazione 3: interfaccia con canali standard
+#include <stdio.h>
+#include <ctype.h>
 
-## Introduzione
 
-Come visto nell'esercitazione precedente, ogni processo ha 3 canali di comunicazione “di base” (può ovviamente aprirne altri durante la sua esecuzione).
-In linguaggio&nbsp;C, questi canali di comunicazione possono essere utilizzati come se fossero dei generici “puntatori a file” (di tipo `FILE *`).
-In particolare, esistono 3&nbsp;costanti disponibili al programmatore:
+int main () 
+{
+   const int MAX = 40;
+   int a = 0;
+   int i = 0;
+   char str[MAX];
+   char ch;
 
-* `stdin`: stream in input,
-* `stdout`: stream in output,
-* `stderr`: stream di errore.
+   printf("Inserire la stringa\n");
 
-Tutti i canali sono già aperti quando il programma inizia la sua esecuzione dentro `main()`.
-È possibile sfruttare i canali sfruttando le funzioni standard di C che operano su file, come `fprintf`, `fscanf`, `fgets` e così via.
+   while( ch != '\n' && a<MAX)
+     {
+	ch = getchar();
+	str[a] = toupper (ch);
+	a++ ;
+     }
+    str[a] = '\0';
 
-Dare un'occhiata al programma `test.c` come esempio.
+    for(i=0; i<a; i++)
+       printf("%c",str[i]);
+	
+    printf("\n") ;
+  
+   return(0);
+}
 
-## Consegna
-
-Scrivere un programma in C che sfrutti i canali standard e che converta tutti i caratteri in input così da renderli maiuscoli in output.
-
-In sostanza, dato il programma della consegna compilato come `consegna`, il comando:
-
-```bash
-echo "Ciao mondo" | consegna
-```
-
-dovrebbe stampare a video “CIAO MONDO”.
